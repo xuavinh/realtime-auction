e967a2e720afb7a3eb2047f56e9e7a868c71b288
Do Xuan Vinh <xuanvinh.1327@gmail.com>
2026-04-20 08:39:39 +0700

    feat: add user model

--- BackEnd/internal/models/user.go
+++ BackEnd/internal/models/user.go
@@ -1,4 +1,12 @@
 package models
 
+import "time"
+
 type User struct {
+	ID           int       `json:"id"`
+	Email        string    `json:"email"`
+	PasswordHash string    `json:"-"`
+	FullName     string    `json:"full_name"`
+	CreatedAt    time.Time `json:"created_at"`
+	UpdatedAt    time.Time `json:"updated_at"`
 }
