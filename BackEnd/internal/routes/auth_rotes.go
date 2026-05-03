a5ac4837283088dfb661e1aa3d3c8cd702dcd41c
Do Xuan Vinh <xuanvinh.1327@gmail.com>
2026-05-03 13:05:49 +0000

    add auth_routes

---
+++ BackEnd/internal/routes/auth_rotes.go
@@ -0,0 +1,20 @@
+package routes
+
+import (
+	"xuanvinh/internal/handler"
+
+	"github.com/gin-gonic/gin"
+)
+
+type AuthDeps struct {
+	Handler *handler.AuthHandler
+}
+
+func RegisterAuthRoutes(rg *gin.RouterGroup, d AuthDeps) {
+	g := rg.Group("/auth")
+
+	g.POST("/register", d.Handler.Register)
+	g.POST("/login", d.Handler.Login)
+	g.POST("/refresh", d.Handler.Refresh)
+	g.POST("/logout", d.Handler.Logout)
+}
