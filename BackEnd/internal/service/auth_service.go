32fff5074c2ea24340bf36ed6d857c191000ff24
Do Xuan Vinh <xuanvinh.1327@gmail.com>
2026-05-03 13:53:49 +0000

    refactor: simplify auth service flow

--- BackEnd/internal/service/auth_service.go
+++ BackEnd/internal/service/auth_service.go
@@ -1,16 +1,32 @@
 package service
 
+import "github.com/gin-gonic/gin"
+
+type userRepo interface {
+}
+
 type AuthService struct {
+	users userRepo
 }
 
-func NewAuthService() *AuthService {
-	return &AuthService{}
+func NewAuthService(users userRepo) *AuthService {
+	return &AuthService{
+		users: users,
+	}
 }
 
-func (s *AuthService) Register() {
+func (s *AuthService) Register(ctx *gin.Context) {
 
 }
 
-func (s *AuthService) Login() {
+func (s *AuthService) Login(ctx *gin.Context) {
+
+}
+
+func (s *AuthService) Refresh(ctx *gin.Context) {
+
+}
+
+func (s *AuthService) Logout(ctx *gin.Context) {
 
 }
