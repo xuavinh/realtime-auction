511a99c310f1bd4e835122fdefaaf8f477a4b89c
Do Xuan Vinh <xuanvinh.1327@gmail.com>
2026-05-03 13:42:43 +0000

    feat(app): add auth_module

---
+++ BackEnd/internal/app/auth_module.go
@@ -0,0 +1,39 @@
+package app
+
+import (
+	"xuanvinh/internal/config"
+	"xuanvinh/internal/handler"
+	"xuanvinh/internal/repository"
+	"xuanvinh/internal/routes"
+	"xuanvinh/internal/service"
+	"xuanvinh/internal/validation"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type AuthModule struct {
+	UserRepo *repository.UserRepository
+	Service  *service.AuthService
+	Handler  *handler.AuthHandler
+	Routes   *routes.AuthModuleRoutes
+}
+
+func BuildAuthModule(pool *pgxpool.Pool, v *validation.Validator, cfg *config.Config) *AuthModule {
+	userRepo := repository.NewUserRepository(pool)
+	authService := service.NewAuthService(userRepo)
+	authHandler := handler.NewAuthHandler(authService, v, cfg)
+	return &AuthModule{
+		UserRepo: userRepo,
+		Service:  authService,
+		Handler:  authHandler,
+		Routes: &routes.AuthModuleRoutes{
+			Register: func(rg *gin.RouterGroup) {
+				routes.RegisterAuthRoutes(rg, routes.AuthDeps{
+					Handler: authHandler,
+				})
+			},
+		},
+	}
+
+}
