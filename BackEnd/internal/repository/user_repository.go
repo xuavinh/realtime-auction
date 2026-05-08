191eaae280f1a90a8312ff15da7dcf5e25aac4c4
Do Xuan Vinh <xuanvinh.1327@gmail.com>
2026-05-08 04:05:12 +0000

    feat(repo): add GetByEmail GetByID

--- BackEnd/internal/repository/user_repository.go
+++ BackEnd/internal/repository/user_repository.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"xuanvinh/internal/db/sqlc"
 
+	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgconn"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
@@ -31,6 +32,28 @@ func (ur *UserRepository) Create(ctx context.Context, arg CreateUserParams) (Cre
 
 func (ur *UserRepository) ExistsEmail(ctx context.Context, email string) (bool, error) {
 	return ur.q.ExistsEmail(ctx, email)
+}
+
+func (ur *UserRepository) GetByEmail(ctx context.Context, email string) (GetUserByEmailRow, error) {
+	row, err := ur.q.GetUserByEmail(ctx, email)
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return GetUserByEmailRow{}, ErrNotFound
+		}
+		return GetUserByEmailRow{}, err
+	}
+	return row, nil
+}
+
+func (ur *UserRepository) GetByID(ctx context.Context, id int32) (GetUserByIDRow, error) {
+	row, err := ur.q.GetUserByID(ctx, id)
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return GetUserByIDRow{}, ErrNotFound
+		}
+		return GetUserByIDRow{}, err
+	}
+	return row, nil
 }
 
 func mapCreateErr(err error) error {
