9472c644b72bfa9f592bf704ef48c4a07458d9a5
Do Xuan Vinh <xuanvinh.1327@gmail.com>
2026-05-09 16:58:25 +0000

    feat(service): add invalid refresh token error

--- BackEnd/internal/service/errors.go
+++ BackEnd/internal/service/errors.go
@@ -6,6 +6,7 @@ import (
 )
 
 var (
-	ErrEmailExists        = utils.NewAppError(http.StatusConflict, "email_exists", "Email already exists")
-	ErrInvalidCredentials = utils.NewAppError(http.StatusUnauthorized, "invalid_credentials", "Invalid email or password")
+	ErrEmailExists         = utils.NewAppError(http.StatusConflict, "email_exists", "Email already exists")
+	ErrInvalidCredentials  = utils.NewAppError(http.StatusUnauthorized, "invalid_credentials", "Invalid email or password")
+	ErrInvalidRefreshToken = utils.NewAppError(http.StatusUnauthorized, "invalid_refresh_token", "Please sign in again")
 )
