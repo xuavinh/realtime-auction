de0f687a3e2e84c25e153c2f2b86e766b458b26b
Do Xuan Vinh <xuanvinh.1327@gmail.com>
2026-05-11 15:22:45 +0000

    feat(repository): add auction type repository

--- BackEnd/internal/repository/types.go
+++ BackEnd/internal/repository/types.go
@@ -10,8 +10,15 @@ type (
 )
 
 // categories
-
 type (
 	ListCategoriesRow  = sqlc.ListCategoriesRow
 	GetCategoryByIDRow = sqlc.GetCategoryByIDRow
+)
+
+// auctions
+type (
+	Auction             = sqlc.Auction
+	AuctionStatus       = string
+	CreateAuctionParams = sqlc.CreateAuctionParams
+	GetAuctionOwnerRow  = sqlc.GetAuctionOwnerRow
 )
